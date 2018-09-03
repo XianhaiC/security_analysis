ce7a3360e52a93517052ddae8c3b84ee03aa3e1b
xianhaic <xianhaic@gmail.com>
2018-09-03 11:06:22 -0700

    WIP intrinsic val function

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import scipy.integrate as integrate
 
 def calculate_iv(latest_fcf, growth_rate_fcf, t, r,):
     pass
@@ -14,3 +15,26 @@ def avg_growth_rate(pv, fv, t):
 # principal_value = future_value / (1 + rate) ^ time
 def discount_future(fv, t, r):
     return fv / (1 + r) ** t
+
+# formula:
+# future_value = pv(1 / ln(r) * (r^t - r) + 1)
+# calculates the continuously compounded value of regular investements of pv
+# for t periods at rate of r
+def compound_invested_earnings(pv, r, t):
+    def f(t, pv, r):
+        return pv(r)**t
+    I = integrate.quad(f, 0, t, args=(pv, r))
+
+    # include the pv since we count the last invested amount as well
+    return I[0] + pv
+
+def intrinsic_val(pv, r, dr, t):
+    # calculated actual growth, factoring in future discount
+    ar = r - dr
+
+    # since we are finding the discounted cash flows/earnings of each
+    # of the next t years, this is equivalent to finding the compounded
+    # regularly invested earnings
+    val = compound_invested_earnings(pv, ar, t)
+
+    return val
